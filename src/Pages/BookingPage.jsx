c47e4e3fd40a0eef9335ed16ac3834c9f7683e71
Ivan Pryymak <ivan.pryymak1@gmail.com>
2026-09-21 14:06:55 -0500

    WIP - working reducer

--- src/Pages/BookingPage.jsx
+++ src/Pages/BookingPage.jsx
@@ -1,3 +1,4 @@
+import { useReducer } from "react";
 import BookingForm from "../Components/BookingForm";
 
 import Footer from '../Components/Footer'
@@ -5,7 +6,20 @@ import Header from '../Components/Header'
 import Hero from '../Components/Hero'
 import Nav from '../Components/Nav'
 
+function initializeTimes(date) {
+    // Simulate fetching available times based on the selected date
+    // For simplicity, we'll return a static list of times for any date
+    return ["17:00", "18:00", "19:00", "20:00", "21:00", "22:00"];
+}
 
+function updateTimes(state, action) {
+    switch (action.type) {
+        case "UPDATE_TIMES":
+            return initializeTimes(action.date);
+        default:
+            return state;
+    }
+}
 
 export default function BookingPage() {
     const [availableTimes, dispatch] = useReducer(updateTimes, [], initializeTimes);
